78b1d485c1156e6e2886ca52bae21b4e8861f572
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2023-12-11 09:34:26 -0300

    Update ArticlesController and README
    Controlador de artículos refactorizado con funciones de comparación de valores e identificación de días. Se agregaron métodos 'conmutables' y 'emparejables' para convertir la representación numérica del día al nombre real del día. También enlaces actualizados en el archivo README.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # php_maipu
 
+Visto nuevo:
+Switch and Match.
+
+
 ## Advertencia
 
 Si bien el código creado por mí tiene licencia MIT, las otras partes, en especial las creadas
@@ -8,7 +12,8 @@ por [Gerardo Fernandez](https://github.com/ger86) tienen sus propias licencias.
 ### Next
 
 
-[Cap 5](https://codersfree.com/courses-status/aprende-php-y-mysql-desde-cero/tipos-de-datos-en-php)
+
+[Cap 13](https://codersfree.com/courses-status/aprende-php-y-mysql-desde-cero/estructuras-repetitivas-ii-for-y-foreach-en-php)
 
 
 [Cap 1](https://www.youtube.com/watch?v=cYCCCgrFSi4&list=PLC8ntN5__iMIAy9V6XO37Dx_bQ5V7zc-h)
--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Service\DefaultService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
@@ -24,22 +24,51 @@ class ArticlesController extends AbstractController
     #[Route(path: '/articulos', name: 'articles', methods: ['GET'])]
     public function list(Request $request): Response
     {
-        $data = $request->query->get('nombre' , 'Gerardo');
+        $data = $request->query->get('nombre', 'Gerardo');
 
-        $a = 30;
-        $b = 15;
+        $firstValue = $a = 3;
+        $secondValue = 4;
 
-        $a += $b;
+        $a += $secondValue;
 
-        return new Response('Bienvenido a PHP Maipú: ' . $data. ',  <br/>'
+        $var_switch = $this->switcheable($firstValue);
+        $var_match = $this->matcheable($secondValue);
+
+        return new Response('Bienvenido a PHP Maipú: ' . $data . ',  <br/>'
             . $this->defaultService->obtenerVersionPhp() . ', <br/>'
-            . $this->defaultService->obtenerSO(). ', <br/>'
-            . $this->defaultService->obtenerPathExtensionPhp(). ', <br/>'
-            .' y ' . $this->defaultService->obtenerPi() . ', <br/>'
-            . 'viendo los datos de $a '. $a
+            . $this->defaultService->obtenerSO() . ', <br/>'
+            . $this->defaultService->obtenerPathExtensionPhp() . ', <br/>'
+            . ' y ' . $this->defaultService->obtenerPi() . ', <br/>'
+            . 'viendo los datos de $a ' . $a . ', <br/>'
+            . ' y determinando si $a es mayor que $b: ' . $this->compararValores($firstValue, $secondValue) . ', <br/>'
+            . ' y determinando si $a es mayor que $b pero anidado: ' . $this->compararValoresAnidados($firstValue, $secondValue). ', <br/>'
+            . $var_switch . ', <br/>'
+            . $var_match . ', <br/>'
+        );
+    }
 
-        )
-            ;
+    /**
+     * Esta función compara los dos valores proporcionados y devuelve un string que indica si el primer valor es mayor o menor que el segundo
+     *
+     * @param int $primerValor El primer valor a comparar
+     * @param int $segundoValor El segundo valor a comparar
+     * @return String "mayor" si el primer valor es mayor que el segundo, "menor" en cualquier otro caso
+     */
+    private function compararValores(int $primerValor, int $segundoValor): string
+    {
+        return $primerValor > $segundoValor ? 'mayor' : 'menor';
+    }
+
+    /**
+     * Esta función compara los dos valores proporcionados de una manera anidada y devuelve un string que indica si el primer valor es mayor, menor o igual al segundo
+     *
+     * @param int $primerValor El primer valor a comparar
+     * @param int $segundoValor El segundo valor a comparar
+     * @return String "mayor" si el primer valor es mayor que el segundo, "menor" si el primer valor es menor que el segundo, "igual" si los dos valores son iguales
+     */
+    private function compararValoresAnidados(int $primerValor, int $segundoValor): string
+    {
+        return $primerValor > $segundoValor ? 'mayor' : ($primerValor < $segundoValor ? 'menor' : 'igual');
     }
 
     #[Route(path: '/articulos-json', name: 'articles-json', methods: ['GET'])]
@@ -47,5 +76,58 @@ class ArticlesController extends AbstractController
     {
 
         return new JsonResponse(['nombre' => 'Gerardo'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Esta función privada 'switcheable' toma un número como entrada y devuelve el día de la semana correspondiente.
+     * Cada número del 1 al 7 representa un día de la semana comenzando por el Lunes.
+     *
+     * @param int $b Un número entero representando el día de la semana, donde 1 es Lunes y 7 es Domingo
+     * @return string Retorno el nombre del día de la semana en español
+     */
+    private function switcheable(int $b): string
+    {
+        switch ($b) {
+            case 1:
+                $day = 'Lunes';
+                break;
+            case 2:
+                $day = 'Martes';
+                break;
+            case 3:
+                $day = 'Miércoles';
+                break;
+            case 4:
+                $day = 'Jueves';
+                break;
+            case 5:
+                $day = 'Viernes';
+                break;
+            case 6:
+                $day = 'Sábado';
+                break;
+            case 7:
+                $day = 'Domingo';
+                break;
+            default:
+                $day = 'Error de código';
+                break;
+        }
+
+        return $day;
+    }
+
+    private function matcheable($b): string
+    {
+        return match ($b) {
+            1 => 'Lunes',
+            2 => 'Martes',
+            3 => 'Miércoles',
+            4 => 'Jueves',
+            5 => 'Viernes',
+            6 => 'Sábado',
+            7 => 'Domingo',
+            default => 'Error de código',
+        };
     }
 }
